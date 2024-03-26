92a3d3a298763e4a5a715f085aedb81bfe604621
Joydip <joydip.mondal@gmail.com>
2024-03-26 09:52:34 +0530

    manual app updated

--- codes/day-2/react-manual-app/src/App.js
+++ codes/day-2/react-manual-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
+    const welcomeMessage = 'Welcome to React JS'
+    const author = 'Created By: Joydip'
     return (
         <div>
             <h2 id="headerElement" style={{ backgroundColor: 'lime' }} className="mystyle">
-                <span>Welcome to React JS</span>
+                <span>{welcomeMessage}</span>
                 <br />
-                <span>Created By: Joydip</span>
+                <span>{author}</span>
             </h2>
             <p>
                 This is a paragraph
--- codes/day-2/react-manual-app/src/index.js
+++ codes/day-2/react-manual-app/src/index.js
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import React from 'react'
 import App from "./App";
 
-const root = createRoot(document.getElementById('container'))
+const container = document.getElementById('container')
+const root = createRoot(container)
 root.render(<App />)
